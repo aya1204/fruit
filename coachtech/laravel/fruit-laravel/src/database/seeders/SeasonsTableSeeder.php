400b415ce08767b4d920ba74f651f4967f9a2545
ayaka <aya_yu7@yahoo.co.jp>
2025-04-09 23:24:17 +0900

    seasonsテーブルのシーディング

---
+++ coachtech/laravel/fruit-laravel/src/database/seeders/SeasonsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SeasonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run():void
+    {
+        $seasons = ['春','夏','秋','冬'];
+            foreach ($seasons as $season) {
+                DB::table('seasons')->insert([
+                    'name' => $season,
+                ]);
+            }
+    }
+}
