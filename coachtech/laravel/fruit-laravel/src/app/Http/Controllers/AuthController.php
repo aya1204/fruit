23bf9265df4cdf30886979c6f37a63fb995e1120
ayaka <aya_yu7@yahoo.co.jp>
2025-04-09 23:06:02 +0900

    商品登録画面用コントローラー

--- coachtech/laravel/fruit-laravel/src/app/Http/Controllers/AuthController.php
+++ coachtech/laravel/fruit-laravel/src/app/Http/Controllers/AuthController.php
@@ -2,19 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Requests\AuthorRequest;
+use App\Models\Product;
 
 class AuthController extends Controller
 {
-    //データ一覧ページの表示
-    public function index ()
-    {
-        return view('index');
-    }
     
-    public function register ()
+    //表示用(GET)
+    public function showRegisterForm()
     {
-        return view('auth/register');
+        return view('auth.register');
+    }
+    //登録処理(POST)
+    public function register (AuthorRequest $request)
+    {
+        $validated = $request->only(['name','price','manual']);
+
+        //季節の処理
+        if($request->has('season')){
+            $validated['season'] = implode(',', $request->season);
+        }
+
+        // 商品画像の保存
+        if ($request->hasFile('image')) {
+
+            $fileName = uniqid() . '.' . $request->file('image')->getClientOriginalExtension();
+
+            $path = $request->file('image')->storeAs('public/fruits-img', $fileName);
+            $validated['image'] = $fileName;
+        } else {
+            return back()->withErrors(['image' => '画像ファイルが選択されていません'])->withInput();
+        }
+
+    // 商品の登録
+    Product::create($validated);
+
+        // 商品一覧ページにリダイレクト
+        return redirect()->route('products.index');
     }
 }
