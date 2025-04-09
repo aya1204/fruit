8547ed91047992ca09d5c21d46ba6fd9859aa7f3
ayaka <aya_yu7@yahoo.co.jp>
2025-04-09 23:16:05 +0900

    バリデーション設定

--- coachtech/laravel/fruit-laravel/src/app/Http/Requests/AuthorRequest.php
+++ coachtech/laravel/fruit-laravel/src/app/Http/Requests/AuthorRequest.php
@@ -24,26 +24,26 @@ class AuthorRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
-            'price' => 'required|integer|between:0,10000',
-            'season' => 'required',
-            'img' => 'required|mimes:png,jpeg',
-            'manual' => 'required|max120',
+            'image' => ['required', 'mimes:png,jpeg'],
+            'name' => ['required'],
+            'price' => ['required','integer','between:0,10000'],
+            'season' => ['required'],
+            'manual' => ['required','max:120'],
         ];
     }
 
     public function messages()
     {
         return [
+            'image.required' => '商品画像を登録してください',
+            'image.mimes' => '「.png」または「.jpeg」形式でアップロードしてください',
             'name.required' => '商品名を入力してください',
             'price.required' => '値段を入力してください',
             'price.integer' => '数値で入力してください',
             'price.between' => '0~10000円以内で入力してください',
             'season.required' => '季節を選択してください',
-            'img.required' => '商品画像を登録してください',
-            'img.mimes:png,jpeg' => '「.png」または「.jpeg」形式でアップロードしてください',
             'manual.required' => '商品説明を入力してください',
-            'manual.max:120' => '120文字以内で入力してください',
+            'manual.max' => '120文字以内で入力してください',
         ];
     }
 }
